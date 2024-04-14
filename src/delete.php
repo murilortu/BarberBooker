0a80b393d406f81e1a7a86da050856d7787b893e
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2024-04-14 18:14:28 -0300

    Operação de deletar criada

--- src/delete.php
+++ src/delete.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "puta merda brother";
+include __DIR__ . "/conexao.php";
+
+$id = $_GET['id'];
+$query = "DELETE FROM Agendamentos WHERE id_agendamento = $id;";
+
+$result = mysqli_query($conexao, $query);
+
+if(!$result)
+    echo "Erro ao executar mysqli_query!";
+else
+    header("Location: ./listar_agendamentos.php");
+?>
--- src/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$numero = 2;
-
-echo "todos são iguais '$numero'";
