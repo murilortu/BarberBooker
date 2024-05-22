b9709bebe7d103c3ae094f9c32ab0c587d362506
murilo.rodrigues <116904598+murilortu@users.noreply.github.com>
2024-05-21 23:50:51 -0300

    Add files via upload

---
+++ BarberBooker/Controllers/adminController.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'Models/AdminModel.php';
+require_once 'Models/AgendamentoModel.php'; // Importe o AgendamentoModel
+require_once 'core/Controller.php';
+require_once 'core/Auth.php';
+
+class AdminController extends Controller {
+    private $adminModel;
+    private $agendamentoModel; // Adicione uma propriedade para o AgendamentoModel
+
+    public function __construct() {
+        parent::__construct();
+        $this->adminModel = new AdminModel();
+        $this->agendamentoModel = new AgendamentoModel(); // Inicialize o AgendamentoModel
+        $this->protegerPagina();
+        if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] != 1) {
+            header('Location: /BarberBooker/login');
+            exit;
+        }
+    }
+
+    public function index() {
+
+        $agendamentoModel = new AgendamentoModel();
+        $agendamentos = $agendamentoModel->listarTodosAgendamentos();
+        $dados = ['agendamentos' => $agendamentos];
+        $this->carregarViewNoTemplate('lista_todos_agendamentos', $dados);
+
+    }
+
+    public function listarUsuarios() {
+        $users = $this->adminModel->getAllUsers();
+        $this->carregarTemplate('admin_listar_agendamento', ['users' => $users]);
+    }
+
+
+}
+?>
