2657940fd3fd2112a0f77746eee27088f13b3a1a
murilo.rodrigues <mrmurilo04@gmail.com>
2024-06-19 00:01:27 -0300

    Atualizando_teste_login

--- tests/LoginModelTest.php
+++ tests/LoginModelTest.php
@@ -15,12 +15,10 @@ class LoginModelTest extends TestCase
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Configuração para lançar exceções em erros SQL
 
         // Criação da tabela Usuarios
-        $this->db->exec("CREATE TABLE Usuarios (id INTEGER PRIMARY KEY, nome TEXT, email TEXT, telefone TEXT, senha TEXT)");
+        $this->db->exec("CREATE TABLE Usuarios (id_usuario INTEGER PRIMARY KEY, nome TEXT, email TEXT, telefone TEXT, senha TEXT)");
 
         // Inserção de um usuário de teste
-        $senhaCriptografada = password_hash('78', PASSWORD_DEFAULT);
-        $stmt = $this->db->prepare("INSERT INTO Usuarios (nome, email, telefone, senha) VALUES ('Murilo', 'teste@gmail.ocm', '8444', :senha)");
-        $stmt->bindParam(':senha', $senhaCriptografada);
+        $stmt = $this->db->prepare("INSERT INTO Usuarios (nome, email, telefone, senha) VALUES ('João Atualizado', 'joaoatualizado@example.com', '987654321', '123')");
         $stmt->execute();
 
         // Inicialização do LoginModel com a conexão do banco de dados mockada
@@ -29,13 +27,15 @@ class LoginModelTest extends TestCase
 
     public function testVerificarCredenciaisCorretas()
     {
-        $result = $this->loginModel->verificarCredenciais('teste@gmail.ocm', '78');
+        $result = $this->loginModel->verificarCredenciais('joaoatualizado@example.com', '123');
         $this->assertTrue($result, 'Login com credenciais corretas deve retornar true');
     }
 
     public function testVerificarCredenciaisIncorretas()
     {
-        $result = $this->loginModel->verificarCredenciais('teste@gmail.ocm', 'senha_incorreta');
+        $result = $this->loginModel->verificarCredenciais('teste@il.ocm', 'senha_incorreta');
         $this->assertFalse($result, 'Login com credenciais incorretas deve retornar false');
     }
 }
+
+
